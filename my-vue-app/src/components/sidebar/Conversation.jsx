527a93bd137f93345683aff813d99848c0ded0cc
Tush786 <tusharsapate34@gmail.com>
2024-10-12 22:09:22 +0530

    sidebar section made form frontend side

---
+++ my-vue-app/src/components/sidebar/Conversation.jsx
@@ -0,0 +1,43 @@
+import { useSocketContext } from "../../context/SocketContext";
+import getLastMessage from "../../utils/getLastMessage";
+
+import useConversation from "../../zustand/useConversation";
+
+const Conversation = ({ conversation, lastIdx, emoji }) => {
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  const isSelected = selectedConversation?._id === conversation._id;
+  const { onlineUsers } = useSocketContext();
+  const isOnline = onlineUsers.includes(conversation._id);
+
+  console.log(getLastMessage(conversation._id));
+
+  return (
+    <>
+      <div
+        className={`flex gap-2 items-center hover:bg-gray-800 rounded p-2 py-1 cursor-pointer
+				${isSelected ? "bg-gray-800" : ""}
+			`}
+        onClick={() => setSelectedConversation(conversation)}
+      >
+        <div className={`avatar ${isOnline ? "online" : ""}`}>
+          <div className="w-12 rounded-full">
+            <img src={conversation.profilePic} alt="user avatar" />
+          </div>
+        </div>
+
+        <div className="flex flex-col flex-1">
+          <div className="flex gap-3 justify-between">
+            <p className="text-md text-gray-200 font-semibold">
+              {conversation.fullName}
+            </p>
+            {/* <span className="indicator-item badge badge-secondary text-white bg-black rounded-md border-0 loading loading-dots loading-md"></span> */}
+          </div>
+        </div>
+      </div>
+
+      {!lastIdx && <div className="divider my-0 py-0 h-1" />}
+    </>
+  );
+};
+export default Conversation;
--- my-vue-app/src/pages/home/Home.jsx
+++ my-vue-app/src/pages/home/Home.jsx
@@ -1,13 +1,13 @@
-// import MessageContainer from "../../components/messages/MessageContainer";
-// import Sidebar from "../../components/sidebar/Sidebar";
+
+import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
   return (
     <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-30">
-      {/* // <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-900"> */}
-      {/* <Sidebar />
-      <MessageContainer /> */}
-      <h1>React App</h1>
+       {/* <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-900"> */}
+       <Sidebar />
+      {/* <MessageContainer />  */}
+      {/* </div> */}
     </div>
   );
 };
